a99515176907e2917b67eb308843570106cae591
Pete Kazmier <pete@kazmier.com>
2013-04-14 02:26:16 -0400

    Added more emacs key bindings ...

--- keys.lua
+++ keys.lua
@@ -10,6 +10,9 @@ function M.enable()
   keys.find_incremental['cs'] = gui.find.find_incremental_next
   keys.find_incremental['\n'] = function() gui.command_entry.finish_mode(nothing) end
 
+  keys.filter_through['\n'] =
+    { gui.command_entry.finish_mode, _M.textadept.filter_through.filter_through }
+
   -- Emacs-like key bindings
   -- do not bind 'cl' (used by other modes), 'ct cl' will redraw
   -- do not bind 'c ' (used for auto completion), set mark is 'cr'
@@ -29,6 +32,7 @@ function M.enable()
   keys['cmh'] = E.move_cut(I.repeatable(buffer.del_word_left)) -- not working
 
   keys['ck'] = E.move_cut(E.line_end)
+  keys['m^'] = _M.textadept.editing.join_lines
 
   keys['cn'] = I.repeatable(buffer.line_down)
   keys['cp'] = I.repeatable(buffer.line_up)
@@ -45,10 +49,15 @@ function M.enable()
   keys['mw'] = E.with_region(E.copy)
   keys['cy'] = buffer.paste
 
+  keys['m|'] = {gui.command_entry.enter_mode, 'filter_through'}
+
   keys['c_'] = I.repeatable(buffer.undo)
 
   -- Custom key bindings can go here
   keys['ct'] = {
+    b = _M.textadept.run.run,
+    f = gui.find.focus,
+    cg = function() I.wrap(buffer.goto_line, I.PROMPT('Goto line:')) end,
     cl = buffer.vertical_centre_caret,
   }
 
@@ -56,10 +65,12 @@ function M.enable()
   keys['cx'] = {
     b = I.switch_buffer,
     k = I.pick_buffer('Kill Buffer:', buffer.close),
+    r = buffer.reload,
     cc = quit,
     cf = io.open_file,
     cr = io.open_recent_file,
     cs = buffer.save,
+    cw = buffer.save_as,
     cx = E.exchange_caret_and_mark,
   }
 
